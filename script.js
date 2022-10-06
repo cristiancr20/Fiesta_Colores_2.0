19d13fb8801f5d1a34f77df64cc919ca6a71ffc7
Cristian Capa <cristian.capa20@gmail.com>
2022-10-05 20:52:38 -0500

    Fiesta Colores

---
+++ script.js
@@ -0,0 +1,70 @@
+function login(){
+  var usuario=document.getElementById("usuario").value;
+  
+  if(usuario == "Cristian"){
+    window.location="home.html";
+  }
+  else if(usuario == "Alejandra"){
+    window.location="home.html";
+  }
+  else if(usuario == "Lisbeth"){
+    window.location="home.html";
+  }
+  else if(usuario == "Alex"){
+    window.location="home.html";
+  }
+  else if(usuario == "Alexis"){
+    window.location="home.html";
+  }
+  else if(usuario == "Erika"){
+    window.location="home.html";
+  }
+  else if(usuario == "Erick"){
+    window.location="home.html";
+  }
+  else if(usuario == "Luis"){
+    window.location="home.html";
+  }
+  else if(usuario == "Anthony"){
+    window.location="home.html";
+  }
+  else if(usuario == "Aracely"){
+    window.location="home.html";
+  }
+  else if(usuario == "José Carlos"){
+    window.location="home.html";
+  }
+  else if(usuario == "Jhon"){
+    window.location="home.html";
+  }
+  else if(usuario == "Marco"){
+    window.location="home.html";
+  }
+  else if(usuario == "Pablo"){
+    window.location="home.html";
+  }
+  else if(usuario == "Javier"){
+    window.location="home.html";
+  }
+  else if(usuario == "Marlon"){
+    window.location="home.html";
+  }
+  else if(usuario == "Maite"){
+    window.location="home.html";
+  }
+  else if(usuario == "Karla"){
+    window.location="home.html";
+  }
+  else if(usuario == "Jorge"){
+    window.location="home.html";
+  }
+  else if(usuario == "Ulbio"){
+    window.location="home.html";
+  }
+  else if(usuario == "Auria"){
+    window.location="home.html";
+  }
+  else{
+    alert("Lo siento, usuario no encontrado");
+  }
+}
